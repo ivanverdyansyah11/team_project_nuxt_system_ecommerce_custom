379c56ca741401123ae44ac353314ba5ffce0747
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-02 09:55:17 +0800

    Progress: Update layout homepage page

---
+++ layouts/homepage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Navbar />
+  <main class="homepage w-100">
+    <slot></slot>
+    <Footer />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
